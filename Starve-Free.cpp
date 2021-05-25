cb1620304ea59b59faa908bbc15652c9176a88f2
Anikait Agrawal <54908236+Anikait143@users.noreply.github.com>
2021-05-25 21:43:42 +0530

    Update Starve-Free.cpp

--- Starve-Free.cpp
+++ Starve-Free.cpp
@@ -1,61 +1,122 @@
-#include<iostream>
-#include<thread>
-#include<mutex>
-#include<math.h>
+#include <iostream>
+using namespace std;
 
-std::mutex reader_mutex, writer_mutex, queue_mutex;
-
-int resource = 1;
-
-unsigned int readers = 0;
-
-void reader(int reader_index)
+struct Node
 {
-    queue_mutex.lock();
-    reader_mutex.lock();
-    if (readers == 0)
-        writer_mutex.lock();
-    readers++;
+    Node* next;
+    pid_t value;
+};
 
-    queue_mutex.unlock();
-    reader_mutex.unlock();
 
-    printf("Reader %d reads resource as %d\n", reader_index,resource);
-
-    reader_mutex.lock();
-    readers--;
-    if (readers == 0)
-        writer_mutex.unlock();
-    reader_mutex.unlock();
-}
-
-void writer(int writer_index)
+class Queue
 {
-    queue_mutex.lock();
-    writer_mutex.lock();
-    queue_mutex.unlock();
-
-    resource = pow(2, writer_index);
-    printf("Writer %d modifies resource as %d\n", writer_index,resource);
-
-    writer_mutex.unlock();
-}
-
-int main()
-{
-    std::thread readers[10], writers[10];
-
-    for (int i=0; i<10; ++i)
+    Node* Front, *Rear;
+    
+    public:
+   	void push(pid_t val)
     {
-        readers[i] = std::thread(reader,i+1);
-        writers[i] = std::thread(writer,i+1);
+        Node* new_process = new Node();
+        new_process->value = val;
+       
+        if(Rear != NULL)
+            Rear->next = new_process;
+        else
+            Front = new_process;
+
+        Rear = new_process;
+    }
+
+    pid_t pop()
+    { 
+
+        if(Front == NULL)
+        {
+            return -1;
+        }
+        else
+        {
+
+            pid_t process_id = Front->value;
+
+            Front = Front->next;
+  
+            if(Front == NULL)
+                Rear = NULL;
+
+            return process_id;
+        }
+    }
+};
+
+class Semaphore
+{
+    
+    int value = 1;
+
+    Queue processes = Queue();
+    
+    public:
+    void wait(pid_t process_id)
+    {
+        value--;
+        if(value < 0)
+        {
+            processes.push(process_id);
+            
+            wait(process_id); 
+        }
     }
     
-    for (int i=0; i<10; ++i)
+    void signal()
     {
-        readers[i].join();
-        writers[i].join();
+        value++;
+        if(value <= 0)
+        {
+            pid_t pid = processes.pop();
+            wakeup(pid); 
+        }
     }
+};
 
-    return 0;
+struct Semaphore rw_queue;
+struct Semaphore resource;
+struct Semaphore r_mutex;
+
+int reader_count = 0;
+
+int data = 1;
+
+void *writer(pid_t process_id){
+    
+    rw_queue.wait(process_id);
+    resource.wait(process_id);
+    rw_queue.signal();
+    
+    data = data*2;
+
+    resource.signal();
+}
+
+void *reader(pid_t process_id){
+    
+    rw_queue.wait(process_id);
+    r_mutex.wait(process_id);
+
+    reader_count++;
+
+    if(reader_count == 1)
+        resource.wait(process_id);
+
+    rw_queue.signal();
+    r_mutex.signal();
+    
+    printf("%d",data);
+    
+    r_mutex.wait(process_id);
+    reader_count--;
+
+    if(reader_count == 0) 
+        resource.signal();
+
+    r_mutex.signal();
 }
